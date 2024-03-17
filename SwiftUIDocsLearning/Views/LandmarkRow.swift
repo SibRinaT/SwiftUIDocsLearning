28eb3fdeaf0a3d4dd090ebcc076844b7a9176057
Ainur <sibrinat616@gmail.com>
2024-03-17 07:39:49 +0300

    fixed bug with images viewing

--- SwiftUIDocsLearning/Views/LandmarkRow.swift
+++ SwiftUIDocsLearning/Views/LandmarkRow.swift
@@ -11,17 +11,18 @@ struct LandmarkRow: View {
     var landmark: Landmark
     
     var body: some View {
-        HStack {
-            landmark.image
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text(landmark.name)
-            
-            Spacer()
-        }
-    }
-}
+          HStack {
+              landmark.image
+                  .resizable()
+                  .frame(width: 50, height: 50)
+              Text(landmark.name)
+
+              Spacer()
+          }
+      }
+  }
 
 #Preview {
     LandmarkRow(landmark: landmarks[0])
 }
+
